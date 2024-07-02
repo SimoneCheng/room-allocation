7cb7c08db78d62453993d08a12a4dffd792aafad
SimoneCheng <bleach1827@gmail.com>
2024-07-02 12:56:27 +0800

    feat: add number-input ui

---
+++ components/number-input/number-input.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { clsx } from 'clsx';
+import { Button } from '../button';
+import { Input } from '../input';
+import { useNumberInput, type UseNumberInputProps } from './use-number-input';
+
+const NumberInput = (props: UseNumberInputProps) => {
+  const {
+    getInputProps,
+    getDecrementButtonProps,
+    getIncrementButtonProps
+  } = useNumberInput(props);
+
+  return (
+    <div className='flex'>
+      <Button
+        className={clsx(
+          'text-2xl',
+          props.value === props.min && 'border-gray-300 text-gray-300 cursor-not-allowed'
+        )}
+        {...getDecrementButtonProps()}
+      >
+        −
+      </Button>
+      <Input
+        className='mx-2 text-base'
+        {...getInputProps()}
+      />
+      <Button
+        className={clsx(
+          'text-2xl',
+          props.value === props.max && 'border-gray-300 text-gray-300 cursor-not-allowed'
+        )}
+        {...getIncrementButtonProps()}
+      >
+        ＋
+      </Button>
+    </div>
+  )
+};
+
+export default NumberInput;
